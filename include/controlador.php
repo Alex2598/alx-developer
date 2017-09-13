e9517f6e68e41309d5c68ebb0eab57e1d5b2173d
AlejandroLopez <program.alopez@gmail.com>
2017-09-12 22:37:17 -0500

    Update information

--- include/controlador.php
+++ include/controlador.php
@@ -3,17 +3,17 @@
 if (!isset($_GET['p'])) {
     $contenido   = 'include/inicio.php';
     $titulo      = 'A L X Developer';
-    $descripcion = 'Mi nombre es Alejandro López Arreola, soy desarrollador Junior, conoce un poco sobre mí.';
-    $palabras    = 'A L X Developer, Programación, Información, Contacto';
+    $descripcion = 'Soy Alejandro López Arreola, Programador Junior, durante tres años he aprendido un sinfín de conocimientos en el mundo de la programación, conoce un poco sobre mí y mi formación profesional.';
+    $palabras    = 'A L X Developer, Programación, Información, Contacto, Lenguajes';
 } else if ($_GET['p'] == 'lenguajes') {
     $contenido   = 'include/lenguajes.php';
     $titulo      = 'A L X Developer | Lenguajes de Programación';
-    $descripcion = 'Mi nombre es Alejandro López Arreola, estos son los lenguajes de programación que he aprendido.';
+    $descripcion = 'Soy Alejandro López Arreola, Programador Junior, los lenguajes de programación pueden ser más interesantes de lo que crees, estos son los que he aprendido.';
     $palabras    = 'A L X Developer, Lenguajes, Programación, Java, Javascript, CSS3, HTML5, Android';
 } else if ($_GET['p'] == 'contacto') {
     $contenido   = 'include/contacto.php';
     $titulo      = 'A L X Developer | Contacto';
-    $descripcion = 'Mi nombre es Alejandro López Arreola, esta es mi información y medio de contacto.';
+    $descripcion = 'Soy Alejandro López Arreola, Programador Junior, en la programación las cosas son más difíciles mientras mas entendemos, permíteme guiarte, este es mi medio de contacto.';
     $palabras    = 'A L X Developer, Contacto, Equipo, Programador, Junior, Trabajo';
 } else {
     $contenido   = 'include/404.php';
